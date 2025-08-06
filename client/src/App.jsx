a38a80eb0eeb816f5a6359df7832b06f3452962b
Aayush Kashyap <kashyapaayush25@gmail.com>
2025-08-06 15:50:41 +0530

    Created Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredits from './pages/BuyCredits'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -13,11 +14,12 @@ const App = () => {
     to-orange-50'>
 
       <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/result' element={<Result/>}/>
-        <Route path='/buy' element={<BuyCredits/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/result' element={<Result/>}/>
+          <Route path='/buy' element={<BuyCredits/>}/>
+        </Routes>
+      <Footer/>
     </div>
   )
 }
